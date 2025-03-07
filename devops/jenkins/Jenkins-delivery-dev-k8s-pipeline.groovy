d8d61e7306992ca1044423bb620bf56b96460a05
MH-Aquiles <aquiles.une.pe@gmail.com>
2025-03-07 14:54:12 -0500

    chore : 20250301 load test jenkins

--- devops/jenkins/Jenkins-delivery-dev-k8s-pipeline.groovy
+++ devops/jenkins/Jenkins-delivery-dev-k8s-pipeline.groovy
@@ -46,7 +46,7 @@ pipeline {
 
     stage('SonarQube Analysis') {
       steps {
-        withSonarQubeEnv('SonarQube') {
+        withSonarQubeEnv('sonarqube') {
           sh 'mvn sonar:sonar -Dsonar.projectKey=app-health-contract-service-v1'
 
         }
